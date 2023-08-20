33b2f42d0cd1bc8fded6f4434497c8b978b75ce8
Ven <currency@gmail.com>
2023-08-19 20:27:18 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="min-h-screen text-zinc-900 dark:text-zinc-50 bg-zinc-100 dark:bg-zinc-800">
+        <header>
+            <div>
+                <nav>
+                    <div class="relative card z-2">
+                        <Menubar :model="items" />
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <RouterView />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+import Menubar from "primevue/menubar";
+
+const items = ref([
+    {
+        label: "Edit",
+        icon: "pi pi-fw pi-pencil",
+        items: [
+            {
+                label: "Left",
+                icon: "pi pi-fw pi-align-left",
+                onclick: () => {
+                    console.log("Left");
+                }
+            },
+            {
+                label: "Right",
+                icon: "pi pi-fw pi-align-right"
+            }
+        ]
+    },
+    {
+        label: "Quit",
+        icon: "pi pi-fw pi-power-off"
+    }
+]);
+</script>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import "./assets/tailwind.css";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Tailwind from "primevue/passthrough/tailwind";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, { unstyled: true, pt: Tailwind });
+
+app.mount("#app");
